56043de77ae6cac9695629a29df70cb41458e3e9
alfredhot <alfredhot@gmail.com>
2016-04-04 15:11:25 +0900

    Initial commit

---
+++ app/src/main/java/com/hot/lib/MainAct.java
@@ -0,0 +1,13 @@
+package com.hot.lib;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainAct extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+    }
+}
